e947d97acde06fc722d8f818474790401d3597e9
sum1982 <66497471+sum1982@users.noreply.github.com>
2023-05-17 17:09:24 -0500

    Update AmitaTest.java

--- amita/src/test/java/AmitaTest.java
+++ amita/src/test/java/AmitaTest.java
@@ -52,7 +52,7 @@ public class AmitaTest {
     public void tearDownMethod() throws Exception {
     }
 
-    @Test
+    @Test (priority =1)
     public void amita() throws Exception {
 
         driver.findElement(By.xpath("//a[@title='Find a Location']")).click();
@@ -79,5 +79,45 @@ driver.findElement(By.xpath("//div[@class='icon']")).click();
         System.out.println(title1);
 
     }
+@Test(priority =2)
+      public void amita1() throws InterruptedException, Exception {
 
+        driver.get("https://healthcare.ascension.org/");
+        driver.findElement(By.linkText("Bill Pay")).click();
+
+//        System.out.println("Before switchint title is =" + driver.getTitle());
+        driver.findElement(By.linkText("PAY A HOSPITAL BILL - VISITPAY PORTAL")).click();
+
+        Set<String> handler = driver.getWindowHandles();
+        Iterator<String> it = handler.iterator();
+
+        String parentWindowId = it.next();
+//        System.out.println("parent window id"+parentWindowId);
+
+        String childWindowId = it.next();
+//        System.out.println("child window id"+childWindowId);
+
+        driver.switchTo().window(childWindowId);
+        System.out.println("chid window Title" + driver.getTitle());
+
+        driver.findElement(By.xpath("//a[contains(text(),'Make a one-time payment')]")).click();
+
+        driver.findElement(By.xpath("//input[@id='AuthenticationId0']")).click();
+        driver.findElement(By.id("AuthenticationId0")).sendKeys(AmitaUtil.getData1().getUser());
+        driver.findElement(By.id("LastName")).click();
+        driver.findElement(By.id("LastName")).sendKeys(AmitaUtil.getData1().getLastname());
+        Select mon = new Select(driver.findElement(By.id("DateOfBirthMonth")));
+        mon.selectByVisibleText("Jan");
+        driver.findElement(By.id("DateOfBirthDay")).sendKeys(AmitaUtil.getData1().getDob());
+        driver.findElement(By.id("DateOfBirthYear")).sendKeys(AmitaUtil.getData1().getDoby());
+        driver.findElement(By.xpath("//label[@id='rbNotPatientLabel']")).click();
+        Select month = new Select(driver.findElement(By.id("PatientDateOfBirthMonth")));
+        month.selectByVisibleText("Jul");
+        driver.findElement(By.id("PatientDateOfBirthDay")).sendKeys(AmitaUtil.getData1().getPdob());
+        driver.findElement(By.id("PatientDateOfBirthYear")).sendKeys(AmitaUtil.getData1().getPdoby());
+
+        driver.findElement(By.xpath("//label[@id='GuestPayAgreeTermsOfUseLabel']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("guestAuthenticationSubmitButton")).click();
+}
 }
